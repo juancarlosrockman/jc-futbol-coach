25c12a4a368e51aa4e8a294b957602ec94bfca7a
juancarlosrockman <juancarlosrockman@gmail.com>
2026-09-22 11:27:48 -0500

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'jcfc-pwa-v1';
+const APP_SHELL = [
+  '/',
+  '/static/logo.jpg',
+  '/static/jc_coach_photo.png',
+  '/static/icon-192.png',
+  '/static/icon-512.png',
+  '/static/apple-touch-icon.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
